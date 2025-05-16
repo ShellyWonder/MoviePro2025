3bd75f1f68ba2b0d372501713a8499045d68db22
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-05-15 20:45:05 -0500

    implemented the chevon button functionality of the actorswiper component via js;

--- Components/UI/ActorSwiper/ActorSwiper.razor.js
+++ Components/UI/ActorSwiper/ActorSwiper.razor.js
@@ -1,1 +1,53 @@
-﻿// ActorSwiper.razor.js 
+﻿// ActorSwiper.razor.js
+
+// This file contains the JavaScript code for the ActorSwiper component.
+// It handles the touch events for swiping left and right on the actor cards.
+export function init(container) {
+    container.querySelector('.nav-btn-next').addEventListener('click', () => {
+        goToNext(container);
+    });
+    container.querySelector('.nav-btn-prev').addEventListener('click', () => {
+        goToPrev(container);
+    });
+}
+
+function goToNext(container) {
+    const btn = container.querySelector('.nav-btn-prev');
+    const btnWidth = btn.getBoundingClientRect().right;
+
+    const contentArea = container.querySelector('.swiper-content');
+
+    const items = contentArea.querySelectorAll('.swiper-item');
+
+    for (let i = 0; i < items.length; i++) {
+        const start = Math.floor(items[i].getBoundingClientRect().left
+        );
+
+        if (start - btnWidth > 5) {
+            contentArea.scrollLeft += start - btnWidth;
+            break;
+        }
+
+    }
+}
+
+function goToPrev(container) {
+    const btn = container.querySelector('.nav-btn-prev');
+    const btnWidth = btn.getBoundingClientRect().right;
+
+    const contentArea = container.querySelector('.swiper-content');
+
+    const items = contentArea.querySelectorAll('.swiper-item');
+
+    for (let i = items.length - 1; i >= 0; i--) {
+        const start = Math.ceil(items[i].getBoundingClientRect().left
+        );
+
+        if (start < (btnWidth - 5)) {
+            contentArea.scrollLeft += start - btnWidth;
+            break;
+        }
+
+    }
+}
+
